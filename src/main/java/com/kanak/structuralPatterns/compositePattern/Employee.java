2622e7fb7822c60bc8f4c19450aaa1aaafe6a5cc
Kanak Arya <kanakarya1922@gmail.com>
2024-03-21 21:06:03 +0530

    Composite design Pattern

---
+++ src/main/java/com/kanak/structuralPatterns/compositePattern/Employee.java
@@ -0,0 +1,11 @@
+package com.kanak.structuralPatterns.compositePattern;
+
+public interface Employee {
+    public  int getId();
+    public String getName();
+    public double getSalary();
+    public void print();
+    public void add(Employee employee);
+    public void remove(Employee employee);
+    public Employee getChild(int i);
+}
